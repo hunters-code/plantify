40b92d22ec888a8a0f66770a9c57a76e39264fef
nashirjamali <nashirjamali@gmail.com>
2025-09-13 01:05:14 +0700

    feat: update gitignore

---
+++ src/plantify_admin/src/declarations/plantify_backend/plantify_backend.did.js
@@ -0,0 +1,413 @@
+export const idlFactory = ({ IDL }) => {
+  const Result_13 = IDL.Variant({ 'ok' : IDL.Bool, 'err' : IDL.Text });
+  const TeamMember = IDL.Record({
+    'id' : IDL.Nat,
+    'linkedin' : IDL.Text,
+    'background' : IDL.Text,
+    'name' : IDL.Text,
+    'role' : IDL.Text,
+    'email' : IDL.Text,
+    'isFounder' : IDL.Bool,
+    'photo' : IDL.Opt(IDL.Text),
+  });
+  const StartupCreationRequest = IDL.Record({
+    'status' : IDL.Text,
+    'periodicProfitSharing' : IDL.Text,
+    'foundedYear' : IDL.Text,
+    'competitiveAdvantage' : IDL.Text,
+    'businessPlan' : IDL.Opt(IDL.Text),
+    'description' : IDL.Text,
+    'sector' : IDL.Text,
+    'useOfFunds' : IDL.Text,
+    'website' : IDL.Text,
+    'teamMembers' : IDL.Vec(TeamMember),
+    'targetMarket' : IDL.Text,
+    'revenueModel' : IDL.Text,
+    'solution' : IDL.Text,
+    'companyLogo' : IDL.Opt(IDL.Text),
+    'companyType' : IDL.Text,
+    'financialProjections' : IDL.Opt(IDL.Text),
+    'marketingStrategy' : IDL.Text,
+    'startupName' : IDL.Text,
+    'fundingGoal' : IDL.Text,
+    'legalDocuments' : IDL.Opt(IDL.Text),
+    'monthlyRevenue' : IDL.Text,
+    'operationalProcess' : IDL.Text,
+    'advisors' : IDL.Text,
+    'nftPrice' : IDL.Text,
+    'location' : IDL.Text,
+    'monthlyExpenses' : IDL.Text,
+    'problemStatement' : IDL.Text,
+    'founderBackground' : IDL.Text,
+  });
+  const Time = IDL.Int;
+  const Startup = IDL.Record({
+    'id' : IDL.Text,
+    'status' : IDL.Text,
+    'periodicProfitSharing' : IDL.Text,
+    'foundedYear' : IDL.Text,
+    'competitiveAdvantage' : IDL.Text,
+    'createdAt' : Time,
+    'businessPlan' : IDL.Opt(IDL.Text),
+    'description' : IDL.Text,
+    'sector' : IDL.Text,
+    'useOfFunds' : IDL.Text,
+    'website' : IDL.Text,
+    'teamMembers' : IDL.Vec(TeamMember),
+    'targetMarket' : IDL.Text,
+    'updatedAt' : Time,
+    'revenueModel' : IDL.Text,
+    'solution' : IDL.Text,
+    'companyLogo' : IDL.Opt(IDL.Text),
+    'founderId' : IDL.Text,
+    'companyType' : IDL.Text,
+    'financialProjections' : IDL.Opt(IDL.Text),
+    'marketingStrategy' : IDL.Text,
+    'startupName' : IDL.Text,
+    'fundingGoal' : IDL.Text,
+    'legalDocuments' : IDL.Opt(IDL.Text),
+    'monthlyRevenue' : IDL.Text,
+    'operationalProcess' : IDL.Text,
+    'advisors' : IDL.Text,
+    'nftPrice' : IDL.Text,
+    'location' : IDL.Text,
+    'monthlyExpenses' : IDL.Text,
+    'problemStatement' : IDL.Text,
+    'founderBackground' : IDL.Text,
+  });
+  const Result_12 = IDL.Variant({ 'ok' : Startup, 'err' : IDL.Text });
+  const CollateralStatus = IDL.Variant({
+    'Active' : IDL.Null,
+    'Released' : IDL.Null,
+    'Locked' : IDL.Null,
+    'Pending' : IDL.Null,
+  });
+  const CollateralTopUp = IDL.Record({
+    'id' : IDL.Text,
+    'status' : IDL.Text,
+    'startupId' : IDL.Text,
+    'timestamp' : Time,
+    'tokenType' : IDL.Text,
+    'amount' : IDL.Nat,
+    'transactionId' : IDL.Opt(IDL.Text),
+  });
+  const CollateralInfo = IDL.Record({
+    'status' : CollateralStatus,
+    'topUpHistory' : IDL.Vec(CollateralTopUp),
+    'startupId' : IDL.Text,
+    'lockEndTime' : IDL.Opt(Time),
+    'createdAt' : Time,
+    'updatedAt' : Time,
+    'tokenType' : IDL.Text,
+    'currentAmount' : IDL.Nat,
+    'requiredAmount' : IDL.Nat,
+    'lockStartTime' : IDL.Opt(Time),
+  });
+  const NFTAccount = IDL.Record({
+    'owner' : IDL.Principal,
+    'subaccount' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+  });
+  const NFTMetadata = IDL.Record({
+    'tokenUri' : IDL.Text,
+    'name' : IDL.Opt(IDL.Text),
+    'description' : IDL.Opt(IDL.Text),
+    'attributes' : IDL.Opt(IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text))),
+    'image' : IDL.Opt(IDL.Text),
+  });
+  const NFTInfo = IDL.Record({
+    'tokenId' : IDL.Nat,
+    'startupId' : IDL.Text,
+    'owner' : NFTAccount,
+    'metadata' : NFTMetadata,
+    'mintedAt' : Time,
+  });
+  const TransferAccount = IDL.Record({
+    'owner' : IDL.Principal,
+    'subaccount' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+  });
+  const BalanceResponse = IDL.Variant({
+    'Error' : IDL.Text,
+    'Success' : IDL.Record({
+      'balance' : IDL.Nat,
+      'account' : TransferAccount,
+      'tokenType' : IDL.Text,
+    }),
+  });
+  const TokenConfig = IDL.Record({
+    'fee' : IDL.Nat,
+    'decimals' : IDL.Nat8,
+    'ledgerId' : IDL.Text,
+    'name' : IDL.Text,
+    'symbol' : IDL.Text,
+    'canisterId' : IDL.Text,
+  });
+  const CollateralProgress = IDL.Record({
+    'status' : IDL.Text,
+    'isFullyPaid' : IDL.Bool,
+    'tokenType' : IDL.Text,
+    'currentAmount' : IDL.Nat,
+    'requiredAmount' : IDL.Nat,
+    'percentage' : IDL.Nat,
+  });
+  const CollateralProgressResponse = IDL.Variant({
+    'Error' : IDL.Text,
+    'Success' : CollateralProgress,
+  });
+  const Result_11 = IDL.Variant({ 'ok' : CollateralInfo, 'err' : IDL.Text });
+  const Result_10 = IDL.Variant({
+    'ok' : IDL.Vec(CollateralTopUp),
+    'err' : IDL.Text,
+  });
+  const NFTConfig = IDL.Record({
+    'permittedDrift' : IDL.Nat,
+    'logo' : IDL.Opt(IDL.Text),
+    'name' : IDL.Text,
+    'maxQueryBatchSize' : IDL.Opt(IDL.Nat),
+    'description' : IDL.Text,
+    'supplyCap' : IDL.Opt(IDL.Nat),
+    'maxTakeValue' : IDL.Opt(IDL.Nat),
+    'atomicBatchTransfers' : IDL.Opt(IDL.Bool),
+    'maxUpdateBatchSize' : IDL.Opt(IDL.Nat),
+    'defaultTakeValue' : IDL.Opt(IDL.Nat),
+    'maxMemoSize' : IDL.Nat,
+    'symbol' : IDL.Text,
+    'txWindow' : IDL.Nat,
+    'canisterId' : IDL.Text,
+  });
+  const EnvironmentConfig = IDL.Record({
+    'nftToken' : NFTConfig,
+    'ckUSDCToken' : TokenConfig,
+    'environment' : IDL.Text,
+    'plantifyAccount' : IDL.Text,
+    'useTestTokens' : IDL.Bool,
+    'icpToken' : TokenConfig,
+  });
+  const Founder = IDL.Record({
+    'id' : IDL.Text,
+    'linkedIn' : IDL.Text,
+    'principal' : IDL.Principal,
+    'createdAt' : Time,
+    'fullName' : IDL.Text,
+    'email' : IDL.Text,
+    'experience' : IDL.Text,
+    'updatedAt' : Time,
+    'idNumber' : IDL.Text,
+    'expertise' : IDL.Text,
+    'taxNumber' : IDL.Text,
+    'address' : IDL.Text,
+    'phone' : IDL.Text,
+    'previousBusinesses' : IDL.Text,
+  });
+  const NFTBalanceResponse = IDL.Variant({
+    'Error' : IDL.Text,
+    'Success' : IDL.Record({ 'balance' : IDL.Nat, 'account' : NFTAccount }),
+  });
+  const Result_9 = IDL.Variant({ 'ok' : NFTBalanceResponse, 'err' : IDL.Text });
+  const Result_8 = IDL.Variant({ 'ok' : NFTInfo, 'err' : IDL.Text });
+  const NFTOwnerResponse = IDL.Variant({
+    'Error' : IDL.Text,
+    'Success' : IDL.Record({
+      'tokenId' : IDL.Nat,
+      'owner' : IDL.Opt(NFTAccount),
+    }),
+  });
+  const Result_7 = IDL.Variant({ 'ok' : NFTOwnerResponse, 'err' : IDL.Text });
+  const Result_6 = IDL.Variant({ 'ok' : IDL.Vec(NFTInfo), 'err' : IDL.Text });
+  const TokenInfoResponse = IDL.Variant({
+    'Error' : IDL.Text,
+    'Success' : IDL.Record({
+      'fee' : IDL.Nat,
+      'decimals' : IDL.Nat8,
+      'name' : IDL.Text,
+      'tokenType' : IDL.Text,
+      'symbol' : IDL.Text,
+    }),
+  });
+  const Result_4 = IDL.Variant({ 'ok' : IDL.Text, 'err' : IDL.Text });
+  const MintNFTRequest = IDL.Record({
+    'startupId' : IDL.Text,
+    'metadata' : NFTMetadata,
+    'memo' : IDL.Opt(IDL.Text),
+    'toAccount' : NFTAccount,
+  });
+  const MintNFTResponse = IDL.Variant({
+    'Error' : IDL.Text,
+    'Success' : IDL.Record({
+      'tokenId' : IDL.Nat,
+      'startupId' : IDL.Text,
+      'transactionId' : IDL.Opt(IDL.Text),
+    }),
+  });
+  const Result_5 = IDL.Variant({ 'ok' : MintNFTResponse, 'err' : IDL.Text });
+  const FounderRegistrationRequest = IDL.Record({
+    'linkedIn' : IDL.Text,
+    'fullName' : IDL.Text,
+    'email' : IDL.Text,
+    'experience' : IDL.Text,
+    'idNumber' : IDL.Text,
+    'expertise' : IDL.Text,
+    'taxNumber' : IDL.Text,
+    'address' : IDL.Text,
+    'phone' : IDL.Text,
+    'previousBusinesses' : IDL.Text,
+  });
+  const Result_3 = IDL.Variant({ 'ok' : Founder, 'err' : IDL.Text });
+  const InvestorRegistrationRequest = IDL.Record({
+    'country' : IDL.Text,
+    'riskTolerance' : IDL.Text,
+    'monthlyBudget' : IDL.Text,
+    'city' : IDL.Text,
+    'fullName' : IDL.Text,
+    'email' : IDL.Text,
+    'investmentGoals' : IDL.Text,
+    'availableCapital' : IDL.Text,
+    'phone' : IDL.Text,
+    'investmentExperience' : IDL.Text,
+  });
+  const Investor = IDL.Record({
+    'id' : IDL.Text,
+    'principal' : IDL.Principal,
+    'country' : IDL.Text,
+    'riskTolerance' : IDL.Text,
+    'monthlyBudget' : IDL.Text,
+    'city' : IDL.Text,
+    'createdAt' : Time,
+    'fullName' : IDL.Text,
+    'email' : IDL.Text,
+    'updatedAt' : Time,
+    'investmentGoals' : IDL.Text,
+    'availableCapital' : IDL.Text,
+    'phone' : IDL.Text,
+    'investmentExperience' : IDL.Text,
+  });
+  const Result_2 = IDL.Variant({ 'ok' : Investor, 'err' : IDL.Text });
+  const TopUpRequest = IDL.Record({
+    'startupId' : IDL.Text,
+    'memo' : IDL.Opt(IDL.Text),
+    'tokenType' : IDL.Text,
+    'amount' : IDL.Nat,
+  });
+  const TopUpResponse = IDL.Variant({
+    'Error' : IDL.Text,
+    'Success' : IDL.Record({
+      'remainingAmount' : IDL.Nat,
+      'newTotal' : IDL.Nat,
+      'isFullyPaid' : IDL.Bool,
+      'tokenType' : IDL.Text,
+      'amount' : IDL.Nat,
+      'topUpId' : IDL.Text,
+      'transactionId' : IDL.Text,
+    }),
+  });
+  const Result_1 = IDL.Variant({ 'ok' : TopUpResponse, 'err' : IDL.Text });
+  const TransferResponse = IDL.Variant({
+    'Error' : IDL.Text,
+    'Success' : IDL.Record({
+      'blockIndex' : IDL.Nat,
+      'tokenType' : IDL.Text,
+      'toAccount' : TransferAccount,
+      'amount' : IDL.Nat,
+      'transactionId' : IDL.Text,
+    }),
+  });
+  const TransferNFTRequest = IDL.Record({
+    'tokenId' : IDL.Nat,
+    'memo' : IDL.Opt(IDL.Text),
+    'toAccount' : NFTAccount,
+  });
+  const TransferNFTResponse = IDL.Variant({
+    'Error' : IDL.Text,
+    'Success' : IDL.Record({
+      'tokenId' : IDL.Nat,
+      'transactionId' : IDL.Opt(IDL.Text),
+    }),
+  });
+  const Result = IDL.Variant({ 'ok' : TransferNFTResponse, 'err' : IDL.Text });
+  const TransferArgs = IDL.Record({
+    'memo' : IDL.Opt(IDL.Text),
+    'tokenType' : IDL.Text,
+    'toAccount' : TransferAccount,
+    'amount' : IDL.Nat,
+  });
+  return IDL.Service({
+    'calculateRequiredCollateral' : IDL.Func(
+        [IDL.Nat, IDL.Text],
+        [IDL.Nat],
+        [],
+      ),
+    'canMintNFT' : IDL.Func([IDL.Text], [Result_13], []),
+    'createStartup' : IDL.Func([StartupCreationRequest], [Result_12], []),
+    'createStartupForFounder' : IDL.Func(
+        [IDL.Text, StartupCreationRequest],
+        [Result_12],
+        [],
+      ),
+    'getAllCollateralInfo' : IDL.Func([], [IDL.Vec(CollateralInfo)], []),
+    'getAllNFTs' : IDL.Func([], [IDL.Vec(NFTInfo)], []),
+    'getAllStartups' : IDL.Func([], [IDL.Vec(Startup)], []),
+    'getBalance' : IDL.Func([TransferAccount, IDL.Text], [BalanceResponse], []),
+    'getCkUSDCBalance' : IDL.Func([TransferAccount], [BalanceResponse], []),
+    'getCkUSDCTokenConfig' : IDL.Func([], [TokenConfig], []),
+    'getCollateralProgress' : IDL.Func(
+        [IDL.Text],
+        [CollateralProgressResponse],
+        [],
+      ),
+    'getCollateralStatus' : IDL.Func([IDL.Text], [Result_11], []),
+    'getCollateralTopUpHistory' : IDL.Func([IDL.Text], [Result_10], []),
+    'getCollectionInfo' : IDL.Func([], [NFTConfig], []),
+    'getEnvironment' : IDL.Func([], [IDL.Text], []),
+    'getEnvironmentConfig' : IDL.Func([], [EnvironmentConfig], []),
+    'getFounders' : IDL.Func([], [IDL.Vec(Founder)], []),
+    'getICPBalance' : IDL.Func([TransferAccount], [BalanceResponse], []),
+    'getICPTokenConfig' : IDL.Func([], [TokenConfig], []),
+    'getNFTBalance' : IDL.Func([NFTAccount], [Result_9], []),
+    'getNFTInfo' : IDL.Func([IDL.Nat], [Result_8], []),
+    'getNFTOwner' : IDL.Func([IDL.Nat], [Result_7], []),
+    'getNFTStats' : IDL.Func(
+        [],
+        [
+          IDL.Record({
+            'totalSupply' : IDL.Nat,
+            'totalStartups' : IDL.Nat,
+            'nextTokenId' : IDL.Nat,
+          }),
+        ],
+        [],
+      ),
+    'getNFTsByStartup' : IDL.Func([IDL.Text], [Result_6], []),
+    'getPlantifyAccount' : IDL.Func([], [IDL.Text], []),
+    'getTokenCanisterId' : IDL.Func([IDL.Text], [IDL.Opt(IDL.Text)], []),
+    'getTokenInfo' : IDL.Func([IDL.Text], [TokenInfoResponse], []),
+    'initializeCollateral' : IDL.Func(
+        [IDL.Text, IDL.Nat, IDL.Text],
+        [Result_4],
+        [],
+      ),
+    'isUsingTestTokens' : IDL.Func([], [IDL.Bool], []),
+    'mintNFT' : IDL.Func([MintNFTRequest], [Result_5], []),
+    'mintNFTForStartup' : IDL.Func([IDL.Text], [Result_4], []),
+    'registerFounder' : IDL.Func([FounderRegistrationRequest], [Result_3], []),
+    'registerInvestor' : IDL.Func(
+        [InvestorRegistrationRequest],
+        [Result_2],
+        [],
+      ),
+    'topUpCollateral' : IDL.Func([TopUpRequest], [Result_1], []),
+    'transferCkUSDC' : IDL.Func(
+        [TransferAccount, IDL.Nat, IDL.Opt(IDL.Text)],
+        [TransferResponse],
+        [],
+      ),
+    'transferICP' : IDL.Func(
+        [TransferAccount, IDL.Nat, IDL.Opt(IDL.Text)],
+        [TransferResponse],
+        [],
+      ),
+    'transferNFT' : IDL.Func([TransferNFTRequest], [Result], []),
+    'transferTokens' : IDL.Func([TransferArgs], [TransferResponse], []),
+    'updateStartupStatus' : IDL.Func([IDL.Text, IDL.Text], [IDL.Bool], []),
+    'whoami' : IDL.Func([], [IDL.Principal], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
