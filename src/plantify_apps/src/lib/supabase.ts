a55aed4c93809a57b72968f80eabff40a432a014
taufiqhidayah <taufiq.hidayah@tiket.com>
2025-10-08 00:10:43 +0700

    fix: feedback

--- src/plantify_apps/src/lib/supabase.ts
+++ src/plantify_apps/src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Initialize the Supabase client
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://gecvpysiaymyyynjhpfz.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlY3ZweXNpYXlteXl5bmpocGZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MDg2NTYsImV4cCI6MjA2OTA4NDY1Nn0.aCO6PYCaW0TGG6mSiRovlmTUjW9HwRdGLqZasapCee0';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 // Create the Supabase client
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
