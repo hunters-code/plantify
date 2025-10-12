ac02647452831616751cdf6d6df4d1c48f163004
Nashir Jamali <nashirjamali@gmail.com>
2025-10-12 21:25:28 +0700

    feat: remove plantify frontend

--- src/plantify_frontend/src/pages/investor/tabs/index.js
+++
@@ -1,4 +0,0 @@
-export { default as OverviewTab } from './OverviewTab';
-export { default as PortfolioTab } from './PortfolioTab';
-export { default as VotingTab } from './VotingTab';
-export { default as TransactionsTab } from './TransactionsTab';
